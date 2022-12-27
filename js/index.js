73fbbcbcb2aafdac67d0206e335db6193cbda4dd
Bob Oyier <boboyier92@gmail.com>
2022-12-27 11:07:50 +0300

    "Resubmit GitHub Search lab. Code works per instructions."

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
+/*
+	We need to create:
+		1. A right div to hold the names of repos.
+		2. A top container to hold both te right div and the github-container div.
+*/
+// let main = document.getElementById("main")
+let resultsContainer = document.getElementById("cont");
+let rightDiv = document.getElementById("right");
+let list = document.getElementById("github-form");
+let repoList = document.getElementById("repo-list");
+
+resultsContainer.appendChild(rightDiv);
+
 function getUser() {
-	let list = document.getElementById("github-form");
 	list.addEventListener("submit", (e) => {
 		e.preventDefault();
 		let user = e.target.search.value;
@@ -7,6 +19,10 @@ function getUser() {
 		fetch(`https://api.github.com/search/users?q=${user}`)
 			.then((response) => response.json())
 			.then(render);
+
+		fetch(`https://api.github.com/users/${user}/repos`)
+			.then((repos) => repos.json())
+			.then(renderRepos);
 	});
 }
 
@@ -23,24 +39,30 @@ function render(data) {
 			We need a LIST in which there will be the:
 				- User's Name
 				- User's Image
-				- User's Link
+				- User's button. This button when clicked should display the
+					user's repository on the right div.
 		
 		*/
 
 		let user = document.createElement("li");
 		let userName = document.createElement("h4");
 		let userImage = document.createElement("img");
-		let userLink = document.createElement("a");
+		let userButton = document.createElement("button");
 
 		userName.textContent = element.login;
 		userImage.src = element.avatar_url;
-		userLink.textContent = `Link to ${element.login}'s GitHub Repositories`;
-		userLink.href = element.html_url;
+		userButton.textContent = `Click to view ${element.login}'s GitHub Repositories`;
+		userButton.id = "bttn";
+
+		// Append container to body then both divs inside container.
+		resultsContainer.appendChild(rightDiv);
+
+		// Append the main list to the left div then users to main list
 
 		// Append all the user attributes inside of the created list called USER:
 		user.appendChild(userName);
 		user.appendChild(userImage);
-		user.appendChild(userLink);
+		user.appendChild(userButton);
 
 		// Append the created USER list inside the Unordered list.
 		ul.appendChild(user);
@@ -56,12 +78,31 @@ function render(data) {
 		userImage.style.border = "1px solid black";
 		userImage.style.borderRadius = "5px";
 
-		userLink.style.display = "block";
-		userLink.style.textAlign = "center";
-		userLink.style.border = "1px solid black";
-		userLink.style.borderRadius = "5px";
-		userLink.style.padding = "3px";
-		userLink.style.fontSize = "18px";
-		userLink.style.textDecoration = "none";
+		userButton.style.display = "block";
+		userButton.style.textAlign = "center";
+		userButton.style.border = "1px solid black";
+		userButton.style.borderRadius = "5px";
+		userButton.style.padding = "10px";
+		userButton.style.fontSize = "18px";
+		userButton.style.textDecoration = "none";
+	});
+}
+
+function renderRepos(data) {
+	let button = document.getElementById("bttn");
+
+	button.addEventListener("click", () => {
+		data.map((element) => {
+			let eachRepo = document.createElement("li");
+			let repoLink = document.createElement("a");
+
+			repoLink.href = element.html_url;
+			repoLink.textContent = element.name;
+
+			repoList.appendChild(eachRepo);
+			eachRepo.appendChild(repoLink);
+		});
+
+		rightDiv.appendChild(repoList);
 	});
 }
